3102a23dd4381db436d530f6295504d71bb1a47f
abhishekrawe <rajputabhisheksingh53@gmail.com>
2022-08-20 19:04:06 +0530

    Intialize all things

---
+++ src/components/layouts/Footer.js
@@ -0,0 +1,92 @@
+import React from 'react'
+import styled from 'styled-components'
+
+
+ function Footer() {
+  return (
+     <FooterContainer className="main-footer">
+      <div className="footer-middle">
+      <div className="container">
+         <div className = "d-flex justify-content-between">
+         
+            <ul className="list-unstyled">
+               <img src="/asset/white.svg" />
+               
+            </ul>
+         
+
+         
+            <ul className="list-unstyled">
+               <li>Contact</li>
+               <li>FAQ's</li>
+               
+            </ul>
+      
+
+       
+            <ul className="list-unstyled">
+               <li>Tutorials</li>
+               <li>Blog</li>
+               
+            </ul>
+      
+
+            
+            <ul className="list-unstyled">
+               <li>Privacy</li>
+               <li>Banned Items</li>
+               
+            </ul>
+         
+
+            
+            <ul className="list-unstyled">
+               <li>About</li>
+               <li>Jobs </li>
+               
+            </ul>
+
+            
+            <ul className="list-unstyled">
+               <li>Facebook</li>
+               <li>Twitter</li>
+               <li>Linkedin</li>
+               
+            </ul>
+         
+
+         </div>
+         <div className="footer-bottom ">
+            <hr/>
+            <div className='d-flex justify-content-between align-items-center'>
+            <p className="text-xs-center">
+             Dukaan {new Date().getFullYear()}, All rights reserved.
+            </p>
+
+            <p> 
+            Made in India <img src="/asset/flag.svg"/>
+            </p>
+            </div>
+            
+         </div> 
+      </div>
+      </div>
+  
+     </FooterContainer>
+  );
+}
+
+export default Footer;
+const FooterContainer = styled.footer`
+.footer-middle {
+   background: var(--mainDark);
+   padding-top : 3rem;
+   color: var(--mainWhite);
+}
+
+.footer-bottom {
+   padding-top: 1rem;
+   padding-bottom: 1rem;
+
+}
+`;
---
+++ src/components/layouts/Navbar.js
@@ -0,0 +1,52 @@
+import React from 'react'
+import styled from 'styled-components'
+
+function Navbar() {
+  return (
+
+<NavContainer className="main-navbar">
+<nav class="navbar navbar-expand-lg bg-primary">
+
+<div class="container container-fluid">
+  <a class="navbar-brand" href="#"> <img src="/asset/white.svg"/></a>
+  <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+      <li class="nav-item">
+        <a class="nav-link text-white text-uppercase" aria-current="page" href="#">Home</a>
+      </li>
+    </ul>
+    <div class="d-flex">
+      <button class="btn bg-primary text-light me-3" >Sign in</button>
+      <button class="btn bg-light text-primary">Dukaan for PC</button>
+    </div>
+  </div>
+</div>
+</nav>
+    </NavContainer>
+    
+  );
+}
+
+
+export default Navbar;
+const NavContainer = styled.nav`
+.navbar{
+  padding-bottom : 200px;
+}
+.bg-primary {
+  background-color : #146EB4 !important;
+
+}
+.text-primary{
+  color : #146EB4 !important;
+}
+
+.container{
+  padding-left : 3rem;
+}
+
+
+`;
